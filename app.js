ad4af93c2e06ad45ca9d7dc203fbb171093f9b1b
Phil Todisco <philtodisco@phils-mbp.lan>
2021-11-02 20:17:08 -0400

    Create RNG for computerPlay

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("hello world")
+// get random choice from computer (rock, paper, or scissors)
+function computerPlay(maxChoices) {
+    return Math.floor(Math.random()* maxChoices)
+}
+
+console.log(computerPlay(3))
+

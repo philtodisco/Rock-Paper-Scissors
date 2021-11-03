88206009591fe9de67276f4e345522058fb0dda0
Phil Todisco <philtodisco@phils-mbp.lan>
2021-11-02 20:54:13 -0400

    Get rock/paper/scissor from RNG

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 // get random choice from computer (rock, paper, or scissors)
-function computerPlay(maxChoices) {
-    return Math.floor(Math.random()* maxChoices)
+
+
+//get random number 0-2
+function computerPlay(max) {
+return Math.floor(Math.random()* max)
+}
+//assign random number a name (rock, paper, or scissors)
+let computerSelection = computerPlay(3)
+if (computerSelection == 0) {
+    computerSelection = "Rock"  
+} else if (computerSelection == 1) {
+    computerSelection = "Paper"
+} else {
+    computerSelection = "Scissors"
 }
 
-console.log(computerPlay(3))
+function playRound(playerSelection, computerSelection) {
 
+}
+
+const playerSelection = "rock"
+
+
+console.log(computerSelection)

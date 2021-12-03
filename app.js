e1b098824215385db2253d8f799039c4ee1743ef
Phil Todisco <philtodisco@phils-mbp.lan>
2021-12-02 21:04:42 -0500

    Added buttons, button returns player selection

--- app.js
+++ app.js
@@ -2,62 +2,89 @@
 let playerScore = 0
 let computerScore = 0
 
-//loop functions for 5 rounds
-for (let i = 0; i < 5; i++) {
-// get selection from computer
-//get random number 0-2
-function computerPlay(max) {
-    return Math.floor(Math.random()* max)
-    }
-    //assign random number a name (rock, paper, or scissors)
-    let computerSelection = computerPlay(3)
-    
-    if (computerSelection == 0) {
-        computerSelection = "Rock"  
-    } else if (computerSelection == 1) {
-        computerSelection = "Paper"
-    } else {
-        computerSelection = "Scissors"
-    }
-    
-    // prompts user for playerSelection
-    const playerSelection = window.prompt("Please enter: Rock, Paper, or Scissors")
-    
-    // function that runs conditionals to determine a winner
-    // takes in 2 args
-    function playRound(playerSelection, computerSelection) {
-        //converts selections to lower case to support non case senstive string comparison 
-        playerSelection = playerSelection.toLowerCase()
-        computerSelection = computerSelection.toLowerCase()
-        //determine winner using condtionals
-        if (playerSelection == "rock" && computerSelection == "paper") {
-            console.log("You lose, paper beats rock!")
-            computerScore++
-        } else if (playerSelection == "rock" && computerSelection == "scissors") {
-            console.log("You win, rock beats scissors!")
-            playerScore++
-        } else if (playerSelection == "paper" && computerSelection == "rock") {
-            console.log("You win, paper beats rock!")
-            playerScore++
-        } else if (playerSelection == "paper" && computerSelection == "scissors") {
-            console.log("You lose, scissor beats paper!")
-            computerScore++
-        } else if (playerSelection == "scissors" && computerSelection == "rock") {
-            console.log("You lose, rock beats scissors!")
-            computerScore++
-        } else if (playerSelection == "scissors" && computerSelection == "paper") {
-            console.log("You win, scissor beats paper!")
-        } else console.log("It's a tie!")
-        console.log(`Player score: ${playerScore}\nComputer Score: ${computerScore}`)
-    }
-    //call function to play round
-    playRound(playerSelection, computerSelection)
+// //loop functions for 5 rounds
+// for (let i = 0; i < 5; i++) {
+
+
+
+
+function playerSelectionFunc() {
+    // targets all <button> elements
+const choices = document.querySelectorAll('button');
+// .forEach method to iterate through each button
+choices.forEach((button) => {
+    // for each one add a 'click' listener
+    button.addEventListener('click', function(e) {
+      choice = e.target.id
+      console.log(choice)
+      return choice
+    });
+  });
 }
 
-//declare winner of all 5 rounds
-if (playerScore > computerScore) {
-    console.log(`You beat the computer ${playerScore} to ${computerScore}`)
-} else if (playerScore < computerScore) {
-    console.log(`The computer beat you ${computerScore} to ${playerScore}`)
-} else console.log(`It's a tie, ${playerScore} to ${computerScore}!`)
+const playerSelection = playerSelectionFunc() 
+
+
+
+
+
+
+
+    
+// // get selection from computer
+// //get random number 0-2
+// function computerPlay(max) {
+//     return Math.floor(Math.random()* max)
+//     }
+//     //assign random number a name (rock, paper, or scissors)
+//     let computerSelection = computerPlay(3)
+    
+//     if (computerSelection == 0) {
+//         computerSelection = "Rock"  
+//     } else if (computerSelection == 1) {
+//         computerSelection = "Paper"
+//     } else {
+//         computerSelection = "Scissors"
+//     }
+    
+//     // prompts user for playerSelection
+//     const playerSelection = window.prompt("Please enter: Rock, Paper, or Scissors")
+    
+//     // function that runs conditionals to determine a winner
+//     // takes in 2 args
+//     function playRound(playerSelection, computerSelection) {
+//         //converts selections to lower case to support non case senstive string comparison 
+//         playerSelection = playerSelection.toLowerCase()
+//         computerSelection = computerSelection.toLowerCase()
+//         //determine winner using condtionals
+//         if (playerSelection == "rock" && computerSelection == "paper") {
+//             console.log("You lose, paper beats rock!")
+//             computerScore++
+//         } else if (playerSelection == "rock" && computerSelection == "scissors") {
+//             console.log("You win, rock beats scissors!")
+//             playerScore++
+//         } else if (playerSelection == "paper" && computerSelection == "rock") {
+//             console.log("You win, paper beats rock!")
+//             playerScore++
+//         } else if (playerSelection == "paper" && computerSelection == "scissors") {
+//             console.log("You lose, scissor beats paper!")
+//             computerScore++
+//         } else if (playerSelection == "scissors" && computerSelection == "rock") {
+//             console.log("You lose, rock beats scissors!")
+//             computerScore++
+//         } else if (playerSelection == "scissors" && computerSelection == "paper") {
+//             console.log("You win, scissor beats paper!")
+//         } else console.log("It's a tie!")
+//         console.log(`Player score: ${playerScore}\nComputer Score: ${computerScore}`)
+//     }
+//     //call function to play round
+//     playRound(playerSelection, computerSelection)
+// // }
+
+// //declare winner of all 5 rounds
+// if (playerScore > computerScore) {
+//     console.log(`You beat the computer ${playerScore} to ${computerScore}`)
+// } else if (playerScore < computerScore) {
+//     console.log(`The computer beat you ${computerScore} to ${playerScore}`)
+// } else console.log(`It's a tie, ${playerScore} to ${computerScore}!`)
 
